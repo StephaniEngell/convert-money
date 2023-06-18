1def167b748585b387eca4b47be40c6ff94c2982
Stephani Engel <stephaniengel18@gmaile.com>
2023-06-18 15:39:53 -0300

    finishing basic logic

--- scripts.js
+++ scripts.js
@@ -32,4 +32,21 @@ function convertValues() {
   }).format(inputCurrencyValue)
 }
 
+function changeCurrency() {
+  const currencyName = document.querySelector(".currency-name")
+  const currencyImage = document.querySelector(".currency-img")
+
+  if (currencySelect.value == "dolar") {
+    currencyName.innerHTML = "Dólar Americano"
+    currencyImage.src = "./assets/dolar.png"
+  }
+  if (currencySelect.value == "euro") {
+    currencyName.innerHTML = "Euro"
+    currencyImage.src = "./assets/euro.png"
+  }
+
+  convertValues()
+}
+
+currencySelect.addEventListener("change", changeCurrency)
 convertButton.addEventListener("click", convertValues)
